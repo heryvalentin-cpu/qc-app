00e2ff63eafddbd4a90dde9d96d06eb8aca06361
heryvalentin-cpu <55792486+heryvalentin-cpu@users.noreply.github.com>
2026-03-04 17:54:38 +0300

    Fix DB config

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 17331
+    port: 3306
 });
 
 db.connect(err => {
